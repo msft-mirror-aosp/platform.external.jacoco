bbe2e9a9b2c9f829bc5e906c218244205c5bfbab
Jeff Gaston <jeffrygaston@google.com>
2017-05-18 13:46:47 -0700

    Add ClassnameMatcher in preparation to pass into Instrumenter for filtering
    Change-Id: I17f66e03a2ae2d2177132e54ffb14815c24b4f13

--- org.jacoco.core.test/src/org/jacoco/core/matcher/WildcardMatcherTest.java
+++ org.jacoco.core.test/src/org/jacoco/core/matcher/WildcardMatcherTest.java
@@ -20,53 +20,53 @@ public class WildcardMatcherTest {
 
 	@Test
 	public void testEmpty() {
-		assertTrue(new WildcardMatcher("").matches(""));
-		assertFalse(new WildcardMatcher("").matches("abc"));
+		assertTrue(new WildcardMatcher("").apply(""));
+		assertFalse(new WildcardMatcher("").apply("abc"));
 	}
 
 	@Test
 	public void testExact() {
-		assertTrue(new WildcardMatcher("abc/def.txt").matches("abc/def.txt"));
+		assertTrue(new WildcardMatcher("abc/def.txt").apply("abc/def.txt"));
 	}
 
 	@Test
 	public void testCaseSensitive() {
-		assertFalse(new WildcardMatcher("abcdef").matches("abcDef"));
-		assertFalse(new WildcardMatcher("ABCDEF").matches("AbCDEF"));
+		assertFalse(new WildcardMatcher("abcdef").apply("abcDef"));
+		assertFalse(new WildcardMatcher("ABCDEF").apply("AbCDEF"));
 	}
 
 	@Test
 	public void testQuote() {
-		assertFalse(new WildcardMatcher("rst.xyz").matches("rstAxyz"));
-		assertTrue(new WildcardMatcher("(x)+").matches("(x)+"));
+		assertFalse(new WildcardMatcher("rst.xyz").apply("rstAxyz"));
+		assertTrue(new WildcardMatcher("(x)+").apply("(x)+"));
 	}
 
 	@Test
 	public void testWildcards() {
-		assertTrue(new WildcardMatcher("*").matches(""));
-		assertTrue(new WildcardMatcher("*").matches("java/lang/Object"));
-		assertTrue(new WildcardMatcher("*Test").matches("jacoco/MatcherTest"));
-		assertTrue(new WildcardMatcher("Matcher*").matches("Matcher"));
-		assertTrue(new WildcardMatcher("Matcher*").matches("MatcherTest"));
-		assertTrue(new WildcardMatcher("a*b*a").matches("a-b-b-a"));
-		assertFalse(new WildcardMatcher("a*b*a").matches("alaska"));
-		assertTrue(new WildcardMatcher("Hello?orld").matches("HelloWorld"));
-		assertFalse(new WildcardMatcher("Hello?orld").matches("HelloWWWorld"));
-		assertTrue(new WildcardMatcher("?aco*").matches("jacoco"));
+		assertTrue(new WildcardMatcher("*").apply(""));
+		assertTrue(new WildcardMatcher("*").apply("java/lang/Object"));
+		assertTrue(new WildcardMatcher("*Test").apply("jacoco/MatcherTest"));
+		assertTrue(new WildcardMatcher("Matcher*").apply("Matcher"));
+		assertTrue(new WildcardMatcher("Matcher*").apply("MatcherTest"));
+		assertTrue(new WildcardMatcher("a*b*a").apply("a-b-b-a"));
+		assertFalse(new WildcardMatcher("a*b*a").apply("alaska"));
+		assertTrue(new WildcardMatcher("Hello?orld").apply("HelloWorld"));
+		assertFalse(new WildcardMatcher("Hello?orld").apply("HelloWWWorld"));
+		assertTrue(new WildcardMatcher("?aco*").apply("jacoco"));
 	}
 
 	@Test
 	public void testMultiExpression() {
-		assertTrue(new WildcardMatcher("Hello:World").matches("World"));
-		assertTrue(new WildcardMatcher("Hello:World").matches("World"));
-		assertTrue(new WildcardMatcher("*Test:*Foo").matches("UnitTest"));
+		assertTrue(new WildcardMatcher("Hello:World").apply("World"));
+		assertTrue(new WildcardMatcher("Hello:World").apply("World"));
+		assertTrue(new WildcardMatcher("*Test:*Foo").apply("UnitTest"));
 	}
 
 	@Test
 	public void testDollar() {
-		assertTrue(new WildcardMatcher("*$*").matches("java/util/Map$Entry"));
+		assertTrue(new WildcardMatcher("*$*").apply("java/util/Map$Entry"));
 		assertTrue(new WildcardMatcher("*$$$*")
-				.matches("org/example/Enity$$$generated123"));
+				.apply("org/example/Enity$$$generated123"));
 	}
 
 }
